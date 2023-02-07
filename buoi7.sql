b4a61121e3664859fc1e5f35403902ad67b62d24
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-02-07 15:17:34 +0700

    Create buoi7.sql

---
+++ buoi7.sql
@@ -0,0 +1,30 @@
+--buoi7
+Use FptAptechBikeStore
+
+CREATE PROCEDURE display_customers 
+as
+SELECT customer_id, first_name
+from sales.customers
+
+EXECUTE display_customers
+
+------
+CREATE database btvnbuoi7
+go 
+use btvnbuoi7
+go
+
+CREATE  TABLE TOYS (
+ProductCode varchar(5) Primary Key,
+TName varchar(30),
+Category varchar(30),
+Manufacturer varchar(30),
+AgeRange varchar(40),
+UnitPrice money,
+Netweight int,
+QtyOnHand int
+);
+
+insert into TOYS values (01, 15, 5445, 654, 3-10, 50.000, 500, 20);  
+insert into TOYS values (02, oto, nhua, hanghoa, 3-10, 20.000, 200, 10);  
+
